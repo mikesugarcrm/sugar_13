62b7a990872ab4990c710f31e5241bd4c1973000
Mike Andersen <mandersen@sugarcrm.com>
2024-06-07 11:36:29 -0700

    Adding mike_c

---
+++ custom/Extension/modules/Accounts/Ext/Language/en_us.lang.php
@@ -0,0 +1,3 @@
+<?php
+// WARNING: The contents of this file are auto-generated.
+$mod_strings['LBL_MIKE_C'] = 'Mike&#039;s Field';
---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_mike_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2024-06-07 18:33:28
+$dictionary['Account']['fields']['mike_c']['labelValue']='Mike\'s Field';
+$dictionary['Account']['fields']['mike_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Account']['fields']['mike_c']['enforced']='';
+$dictionary['Account']['fields']['mike_c']['dependency']='';
+$dictionary['Account']['fields']['mike_c']['required_formula']='';
+$dictionary['Account']['fields']['mike_c']['readonly_formula']='';
+
+ ?>
