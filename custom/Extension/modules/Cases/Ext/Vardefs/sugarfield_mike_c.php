f58b4ed477f67b79b7e1b3f92a8f5acc1ffde18b
Mike Andersen <mandersen@sugarcrm.com>
2024-11-18 07:51:10 -0800

    Added the mike_c field to the Cases module.

---
+++ custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_mike_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2024-11-18 15:39:32
+$dictionary['Case']['fields']['mike_c']['labelValue']='Mike\'s Comments';
+$dictionary['Case']['fields']['mike_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Case']['fields']['mike_c']['enforced']='';
+$dictionary['Case']['fields']['mike_c']['dependency']='';
+$dictionary['Case']['fields']['mike_c']['required_formula']='';
+$dictionary['Case']['fields']['mike_c']['readonly_formula']='';
+
+ ?>
