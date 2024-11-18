4a1a48377509c1891c6863b77b0ef9ee38beec9c
Mike Andersen <mandersen@sugarcrm.com>
2024-11-18 07:59:00 -0800

    Added mike_c to the Tasks module.

---
+++ custom/Extension/modules/Tasks/Ext/Vardefs/sugarfield_mike_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2024-11-18 15:56:53
+$dictionary['Task']['fields']['mike_c']['labelValue']='Mike\'s Text';
+$dictionary['Task']['fields']['mike_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Task']['fields']['mike_c']['enforced']='';
+$dictionary['Task']['fields']['mike_c']['dependency']='';
+$dictionary['Task']['fields']['mike_c']['required_formula']='';
+$dictionary['Task']['fields']['mike_c']['readonly_formula']='';
+
+ ?>
