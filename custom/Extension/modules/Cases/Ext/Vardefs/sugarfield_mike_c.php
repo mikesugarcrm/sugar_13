b927b1e38b785a94757f3b27fa98704db844d26d
Mike Andersen <mandersen@sugarcrm.com>
2025-03-28 11:06:06 -0700

    Add new field for Cases.

---
+++ custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_mike_c.php
@@ -0,0 +1,14 @@
+<?php
+ // created: 2025-03-28 18:04:00
+$dictionary['Case']['fields']['mike_c']['labelValue']='Mike\'s Field';
+$dictionary['Case']['fields']['mike_c']['full_text_search']=array (
+  'enabled' => '0',
+  'boost' => '1',
+  'searchable' => false,
+);
+$dictionary['Case']['fields']['mike_c']['enforced']='';
+$dictionary['Case']['fields']['mike_c']['dependency']='';
+$dictionary['Case']['fields']['mike_c']['required_formula']='';
+$dictionary['Case']['fields']['mike_c']['readonly_formula']='';
+
+ ?>
